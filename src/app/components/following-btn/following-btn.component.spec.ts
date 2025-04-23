6de0c03607ed50d4ac8fe532f691ef16a8e24aca
lironhefcode <lironheftman@gmail.com>
2025-04-24 01:17:43 +0300

    following btn tests

--- src/app/components/following-btn/following-btn.component.spec.ts
+++ src/app/components/following-btn/following-btn.component.spec.ts
@@ -19,5 +19,14 @@ describe('FollowingBtnComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
+  })
+  it('should emit unFollow event on button click', () => {  
+    spyOn(component.unFollow, 'emit')
+    spyOn(component, 'onClick').and.callThrough()
+    const button = fixture.nativeElement.querySelector('button')
+    button.click();
+    fixture.detectChanges()
+    expect(component.onClick).toHaveBeenCalled()
+    expect(component.unFollow.emit).toHaveBeenCalled();
+  })
 });
