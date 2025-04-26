67d04ebe4a1061551e75f757a1f1b64b107bf3e2
lironhefcode <lironheftman@gmail.com>
2025-04-26 12:16:07 +0300

    auth guard test

--- src/app/guards/auth-guard.guard.spec.ts
+++ src/app/guards/auth-guard.guard.spec.ts
@@ -1,17 +1,60 @@
-import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivateFn, Router, RouterState, RouterStateSnapshot } from '@angular/router';
 
 import { authGuard } from './auth-guard.guard';
-
+import { AuthService } from '../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BehaviorSubject, delay, Observable, of } from 'rxjs';
+import { User } from '../models/userInterface';
 describe('authGuardGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  let activatedRoute : ActivatedRoute
+  const mockRouter = jasmine.createSpyObj('Router', ['createUrlTree','navigate'])
 
+  const mockUser : User = {
+    _id: 'testId',
+    username: 'testUser',
+    password: 'testPassword',
+    fullname: 'testFullname',
+    imgUrl: 'test.png',
+    stories: [],
+    following: [],
+    followers: [],
+    likedStoryIds: [],
+    savedStoryIds: []
+  }
+  const mockAuthService ={
+    currentUser$: new BehaviorSubject<User | null>(mockUser)
+  }
+  
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{provide: Router ,useValue: mockRouter }
+        ,{provide : ActivatedRoute, useValue : { snapshot : {}}},
+        {provide : AuthService, useValue : mockAuthService},
+        ]
+    })
+    
+    activatedRoute =  TestBed.inject(ActivatedRoute);
+    mockRouter.createUrlTree.calls.reset()
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
-  });
-});
+  
+  it('should return true', fakeAsync( () => {
+    const result =  TestBed.runInInjectionContext(() => authGuard(null!, null!)) as Observable<boolean>;
+    let guardOutpot = null
+    result.subscribe((res) =>{guardOutpot = res} );
+    tick();
+    expect(guardOutpot).toBeTrue();
+}));
+it('should return false',fakeAsync( () => {
+  mockAuthService.currentUser$.next(null)
+  tick()
+  const result =  TestBed.runInInjectionContext(() => authGuard(null!, null!)) as Observable<boolean>;
+  let guardOutpot = null
+  result.subscribe((res) => guardOutpot = res);
+  tick()
+  expect(guardOutpot).toBeFalse();
+})  )
+})
--- src/app/guards/auth-guard.guard.ts
+++ src/app/guards/auth-guard.guard.ts
@@ -3,8 +3,8 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { map, tap } from 'rxjs';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = (route , state) => {
   const authService = inject(AuthService)
   const router  = inject(Router)
-  return authService.currentUser$.pipe( map(user  => !! user || router.createUrlTree([''])))
+  return authService.currentUser$.pipe( map(user  => !!user ))
 };
