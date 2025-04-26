7285acfe1d88f1b836cb7bec5179ed14cfe060cf
lironhefcode <lironheftman@gmail.com>
2025-04-26 12:31:29 +0300

    feed component test

--- src/app/pages/feed/feed.component.spec.ts
+++ src/app/pages/feed/feed.component.spec.ts
@@ -5,11 +5,11 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { StoreisService } from '../../services/storeis.service';
 import { CreatePostModalComponent } from '../../components/create-post-modal/create-post-modal.component';
 import { RouterOutlet } from '@angular/router';
+import { signal } from '@angular/core';
 
 describe('FeedComponent', () => {
   let component: FeedComponent;
   let fixture: ComponentFixture<FeedComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [FeedComponent,HttpClientTestingModule,CreatePostModalComponent,RouterOutlet],
@@ -19,10 +19,28 @@ describe('FeedComponent', () => {
 
     fixture = TestBed.createComponent(FeedComponent);
     component = fixture.componentInstance;
+    component.showCreateModel = signal(false)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
+  })
+  it('should disoplay create post modal', () => {
+    component.showCreateModel.set(true)
+    fixture.detectChanges()
+    const modal = fixture.nativeElement.querySelector('create-post-modal')
+    expect(modal).toBeTruthy()
+  })
+  it('should ont display create post modal', () => {
+    const modal = fixture.nativeElement.querySelector('create-post-modal') as HTMLElement
+    expect(modal.hidden).toBe(true)
+  })
+  it('should load stories on init', () => {
+    const storeisService = TestBed.inject(StoreisService)
+    const loadStoriesSpy = spyOn(storeisService, 'loadStories').and.callThrough()
+    component.ngOnInit()
+    expect(loadStoriesSpy).toHaveBeenCalled()
+  })
+
 });
